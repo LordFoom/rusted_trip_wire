e1fc0456e5a0b9a2f49d2aaed33d3c42e463b2aa
lordfoom <lordfoom@gmail.com>
2024-03-16 18:41:34 +0200

    Stub example of notify crate

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 use clap::Parser;
+use anyhow::Result;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args{
     ///This is the file to watch for any changes
     #[arg(short, long, value_name="FILE")]
-    watch_file: String,
+    file_to_watch: String,
     ///This is the command that will be fired if the file changes
     #[arg(short, long, value_name="COMMAND")]
-    trigger_command: String,
+    command: String,
 }
-fn main() {
-    println!("Hello, world!");
+fn main()->Result<()> {
+    //oh, there is a cool library we can use
+    let mut watcher = notify::recommended_watcher(|res|{
+        match res {
+            Ok(event) => println!("event: {}", event),
+            Err(e) => println!("watch error: {}", e),
+        }
+    })?;
+    watcher.watch(Path::new("."), RecursiveMode::Recursive)?;
 }
