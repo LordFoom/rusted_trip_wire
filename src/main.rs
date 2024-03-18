aa19f824bd66bc254088806cdc0f9b98d9b599c9
Timothy Spring <foom@amazon.com>
2024-03-18 17:52:42 +0200

    Basic  watching
    Does nothing...yet

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,43 @@
 use clap::Parser;
-use anyhow::Result;
-use notify::{Config, PollWatcher, RecursiveMode, Watcher};
-use std::path::Path;
+use notify::RecommendedWatcher;
+use notify::{Config, RecursiveMode, Watcher};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
-struct Args{
+struct Args {
     ///This is the file to watch for any changes
-    #[arg(short, long, value_name="FILE")]
-    file_to_watch: String,
+    #[arg(short, long, value_name = "FILE")]
+    path_to_watch: String,
     ///This is the command that will be fired if the file changes
-    #[arg(short, long, value_name="COMMAND")]
-    command: String,
+    #[arg(short, long, value_name = "COMMAND")]
+    command: Option<String>,
 }
-fn main()->Result<()> {
+fn main() -> notify::Result<()> {
+    let args = Args::parse();
     //oh, there is a cool library we can use
-    let mut watcher = notify::recommended_watcher(|res|{
+    // let mut watcher = notify::recommended_watcher(|res| match res {
+    //     Ok(event) => println!("event: {:?}", event),
+    //     Err(e) => println!("watch error: {}", e),
+    // })?;
+    // watcher.watch(Path::new(&args.path_to_watch), RecursiveMode::Recursive)?;
+    if let Err(e) = watch(&args.path_to_watch) {
+        println!("We had a terrible error! {}", e);
+    }
+
+    Ok(())
+}
+
+fn watch(path: &str) -> notify::Result<()> {
+    let (tx, rx) = std::sync::mpsc::channel();
+    let mut watcher = RecommendedWatcher::new(tx, Config::default())?;
+    watcher.watch(path.as_ref(), RecursiveMode::Recursive);
+
+    for res in rx {
         match res {
-            Ok(event) => println!("event: {:?}", event),
-            Err(e) => println!("watch error: {}", e),
+            Ok(event) => println!("Event! {event:?}"),
+            Err(err) => println!("ERRRROOORRRRR {err:?}"),
         }
-    })?;
-    watcher.watch(Path::new("."), RecursiveMode::Recursive)?;
+    }
 
     Ok(())
 }
